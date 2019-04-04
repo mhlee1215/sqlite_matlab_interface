a937f161976ae9f34a6a77c6be049e828a1aba07
mhlee1215 <minhaenl@uci.edu>
2019-04-03 18:43:24 -0700

    added

--- read_data.m
+++ read_data.m
@@ -6,3 +6,23 @@ result = mksqlite(dbid, 'select * from reviews');
 
 star = {result.star};
 comments = {result.comments};
+shop_url = {result.shop_url};
+
+
+result2 = mksqlite(dbid, 'select * from shops');
+city = {result2.city};
+shop_url2 = {result2.url};
+
+
+m = containers.Map; 
+for i = 1 : length(result2)
+    m(shop_url2{i}) = city{i};
+end
+
+
+m(shop_url{5000})
+
+
+
+    
+
